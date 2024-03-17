3bbd603c5bb9b1c4eb7eec0e782db232ee7c7902
vngarg <sngargrsd@gmail.com>
2024-03-17 18:52:38 +0530

    Updated Frontend Config URL

--- frontend/src/utils/Config.js
+++ frontend/src/utils/Config.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://192.168.149.182:4000'
+  baseURL: 'https://s3k-solutions.onrender.com'
 });
 
 axiosInstance.interceptors.request.use(async function (config) {
